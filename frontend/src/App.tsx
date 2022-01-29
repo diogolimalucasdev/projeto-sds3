4f01267510339a4185a8ad64a6841ecf422238bb
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-01-29 11:07:23 -0300

    Components estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+/* shift + alt + a para comentar */
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary"> Ola mundo</h1>
-    </div>
+    /* Esse comando ( linha 9 <>  linha 18 </>) é o fragment, serve para colocar tudoem um elemento só*/
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary"> Ola mundo</h1>
+        <DataTable></DataTable>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+       
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/diogolimalucasdev" target="blank" rel="diogolimalucas">Diogo Lima Lucas</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior, Senai: <a href="https://www.linkedin.com/in/diogolimadev/" target="blank" rel="diogolimalucas">diogolimalucasdev</a></small></p>
+                </div>
+            </footer>
+        
+    );
+}
+
+export default Footer;
