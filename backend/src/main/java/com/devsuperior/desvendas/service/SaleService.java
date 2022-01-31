58abf7a3df34acae305e4648a52fbb5c460a745b
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-01-30 21:23:32 -0300

    pesquisar por grupo

--- backend/src/main/java/com/devsuperior/desvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/desvendas/service/SaleService.java
@@ -3,6 +3,8 @@ package com.devsuperior.desvendas.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.desvendas.dto.SaleDTO;
+import com.devsuperior.desvendas.dto.SaleSuccessDTO;
+import com.devsuperior.desvendas.dto.SaleSumDTO;
 import com.devsuperior.desvendas.entities.Sale;
 import com.devsuperior.desvendas.repositories.SaleRepository;
 import com.devsuperior.desvendas.repositories.SellerRepository;
@@ -30,5 +34,17 @@ public class SaleService {
 			Page<Sale> result = repository.findAll(pageable);
 			return result.map(x -> new SaleDTO(x));
 		}
+		
+		@Transactional(readOnly = true)
+		public List<SaleSumDTO> amountGroupedBySeller(){
+			return repository.amountGroupedBySeller();
+		}
+		
+		@Transactional(readOnly = true)
+		public List<SaleSuccessDTO> successGroupedBySeller(){
+			return repository.successGroupedBySeller();
+		}
+		
+
 }
 
