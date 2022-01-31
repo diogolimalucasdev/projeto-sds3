b6b8d87af88bc0fbcb5fe93111c22d3f7b0beaf9
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-01-31 10:17:47 -0300

    primeira requisição

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,13 +1,34 @@
 /* shift + alt + a para comentar */
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
+    
+    // forma errada
+    let chartData : ChartData = {labels: [], series: []};
 
+    axios.get(`${BASE_URL} + '/sales/amount-by-seller'`)
+        .then(response => {
 
-    const mockData = {
+            const data =  response.data as SaleSum[];
+            const myLabels = data.map(x => x.selleName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels: myLabels, series: mySeries};
+            console.log(chartData)
+        });
+
+   /*  const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    } 
+    */
 
     const options = {
         legend: {
@@ -22,8 +43,8 @@ const DonutChart = () => {
         <Chart
             /* me referencio ao options criado no incio */
             /* os 3 pontos pega o objeto do jeito que esta e depois da virgula eu aiciono as categorias  */
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
